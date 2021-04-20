a65ac6b1f329533c4fba1538145a6134b19defce
Edvaldo Afonso <eadafonso@gmail.com>
2021-04-20 09:34:58 +0100

    feat: configure Project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import TouchScreen from "./src/screens/TouchScreen";
+import PinScreen from "./src/screens/PinScreen";
 
 export default function App() {
+  const AppStack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AppStack.Navigator headerMode="none">
+        <AppStack.Screen name="Touch" component={TouchScreen} />
+        <AppStack.Screen name="Pin" component={PinScreen} />
+      </AppStack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
